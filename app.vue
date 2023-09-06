bd9278e6001d2c17a6c5f27f5a36af4d13b4dbc0
AllenYu0118 <yulei0118@gmail.com>
2023-09-06 23:38:32 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <button @click="testClassObject">classObject</button>
+    </div>
+    <div>
+      <button @click="testNormalObject">normalObject</button>
+    </div>
+    <div>
+      <button @click="testClassObjectInNormalObject">
+        classObjectInNormalObject
+      </button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+class Temp {
+  test = 'hi';
+}
+
+const classObject = new Temp();
+const normalObject = {
+  test: 'hi',
+};
+const classObjectInNormalObject = {
+  test: classObject,
+};
+
+const testClassObject = () => {
+  $fetch<number>('/api/temp', {
+    method: 'post',
+    body: classObject,
+  });
+};
+const testNormalObject = () => {
+  $fetch<number>('/api/temp', {
+    method: 'post',
+    body: normalObject,
+  });
+};
+const testClassObjectInNormalObject = () => {
+  $fetch<number>('/api/temp', {
+    method: 'post',
+    body: classObjectInNormalObject,
+  });
+};
+</script>
