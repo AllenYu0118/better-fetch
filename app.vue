f96dd961e8d1fa95a173cac8bc0b389199003df4
AllenYu0118 <yulei0118@gmail.com>
2023-09-07 00:36:54 +0800

    2 files modified

--- app.vue
+++ app.vue
@@ -9,6 +9,15 @@ const getData = () => {
     headers: {
       Token: 'better-fetch-client',
     },
+    onRequest(context) {
+      const { request, options } = context;
+      context.request += '?a=1';
+      console.log('[fetch request]', request, options.headers.Token);
+      options.query = { a: 1, b: 2 };
+    },
+    onResponse({ request, response, options }) {
+      console.log('[fetch response]', request, response.status, options.query);
+    },
   });
 };
 </script>
--- utils/better-fetch/fetch.ts
+++ utils/better-fetch/fetch.ts
@@ -1,24 +1,41 @@
 export type $Fetch = (
   request: string | Request,
-  options?: RequestInit
+  options?: FetchOptions
 ) => Promise<Response>;
 
-interface Context {
-  request: RequestInfo | undefined;
-  options: RequestInit;
+interface FetchOptions extends RequestInit {
+  onRequest?(context: FetchContext): Promise<void> | void;
+  onResponse?(context: FetchContext): Promise<void> | void;
 }
-const context: Context = {
+
+interface FetchContext {
+  request?: RequestInfo;
+  options: FetchOptions;
+  response?: Response;
+}
+const context: FetchContext = {
   request: undefined,
   options: {},
+  response: undefined,
 };
 
 const $fetch: $Fetch = async (_request, _options = {}) => {
   context.request = _request;
   context.options = { ...context.options, ..._options };
-  return await fetch(context.request, context.options);
+
+  if (context.options?.onRequest) {
+    await context.options?.onRequest(context);
+  }
+  context.response = await fetch(context.request, context.options);
+
+  if (context.options?.onResponse) {
+    await context.options?.onResponse(context);
+  }
+
+  return context.response;
 };
 
-export function createFetch(globalOptions: RequestInit = {}) {
+export function createFetch(globalOptions: FetchOptions = {}) {
   context.options = globalOptions;
   return $fetch;
 }
