8b7a423d6680039f4efa5eb358d5118c4b3c65b7
AllenYu0118 <yulei0118@gmail.com>
2023-09-07 15:32:54 +0800

    1 file modified

--- app.vue
+++ app.vue
@@ -1,15 +1,57 @@
 <template>
   <div>
-    <button @click="getData">getData</button>
+    <div>
+      <h3>Fetch</h3>
+      <button @click="simpleFetch">simple fetch</button>
 
-    <p><button @click="getText">getText</button></p>
+      <p><button @click="composableFetch">composable fetch</button></p>
+    </div>
 
-    <p><button @click="fetchTimeout">fetchTimeout 5s</button></p>
+    <div>
+      <h3>Better Fetch</h3>
+      <button @click="getData">getData</button>
 
-    <p><button @click="autoRetry">autoRetry</button></p>
+      <p><button @click="getText">getText</button></p>
+
+      <p><button @click="fetchTimeout">fetchTimeout 5s</button></p>
+
+      <p><button @click="autoRetry">autoRetry</button></p>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+const simpleFetch = async () => {
+  fetch('/api/getText')
+    .then((response) => response.text())
+    .then((text) => console.log('text: ', text));
+};
+
+const composableFetch = async () => {
+  const headers = new Headers({
+    token: 'bigStron',
+  });
+
+  const abortController = new AbortController();
+
+  const request = new Request('/api/getData', {
+    credentials: 'omit',
+    mode: 'cors',
+    headers: headers,
+    signal: abortController.signal,
+  });
+
+  fetch(request)
+    .then((response) => response.json())
+    .then((json) => console.log('json: ', json))
+    .catch((err) => {
+      console.error(`fetch error: ${err.message}`);
+    });
+
+  setTimeout(() => {
+    abortController.abort();
+  }, 5000);
+};
+
 const getData = async () => {
   const data = await $bfetch('/api/getData', {
     headers: {
