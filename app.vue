83ae024397e63d10ccfd8f1abfdcc91654963f54
AllenYu0118 <yulei0118@gmail.com>
2023-09-07 12:25:25 +0800

    3 files modified, 1 added

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <button @click="getData">getData</button>
+
+    <p><button @click="getText">getText</button></p>
   </div>
 </template>
 <script setup lang="ts">
-const getData = () => {
-  $bfetch('/api/getData', {
+const getData = async () => {
+  const data = await $bfetch('/api/getData', {
     headers: {
       Token: 'better-fetch-client',
     },
@@ -19,5 +21,13 @@ const getData = () => {
       console.log('[fetch response]', request, response.status, options.query);
     },
   });
+
+  console.log(data);
+};
+
+const getText = async () => {
+  const data = await $bfetch('/api/getText');
+
+  console.log(data);
 };
 </script>
